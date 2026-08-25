e17d4abcbf6aeb661fac9f19046358e067d7e54c
Thomas Rousseau <thomas.rousseau19@gmail.com>
2026-08-25 15:35:45 +0200

    test:  fail CI check

--- index.test.js
+++ index.test.js
@@ -3,5 +3,5 @@ const assert = require("node:assert");
 const addition = require("./index");
 
 test("addition of 2 and 3 returns 5", () => {
-    assert.strictEqual(addition(2, 3), 5);
+    assert.strictEqual(addition(2, 3), 6);
 });
